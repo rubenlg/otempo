4cbbf0991972f15df75c2b32f32d5487f9f57558
Ruben <ryu@gpul.org>
2018-09-14 17:52:15 +0200

    Fix a couple of issues in the station widget: - The first column had black text. - With the API update it was using too much padding and jumping to 2 rows. - Format the code of the widget provider.

--- app/src/main/java/org/otempo/view/StationWidget.java
+++ app/src/main/java/org/otempo/view/StationWidget.java
@@ -1,34 +1,22 @@
 /*
  * Copyright (C) 2010-2011 Ruben Lopez
- * 
+ *
  * This file is part of OTempo - Galician Weather
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/>.
  */
 package org.otempo.view;
-
-import java.util.Calendar;
-import java.util.Date;
-
-import org.otempo.R;
-import org.otempo.StationManager;
-import org.otempo.model.Station;
-import org.otempo.model.StationMediumTermPrediction;
-import org.otempo.model.StationPrediction;
-import org.otempo.model.StationShortTermPrediction;
-import org.otempo.util.DateUtils;
-import org.otempo.util.ResourceUtils;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -40,25 +28,37 @@ import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
+
+import org.otempo.R;
+import org.otempo.StationManager;
+import org.otempo.model.Station;
+import org.otempo.model.StationMediumTermPrediction;
+import org.otempo.model.StationPrediction;
+import org.otempo.model.StationShortTermPrediction;
+import org.otempo.util.DateUtils;
+import org.otempo.util.ResourceUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Proveedor del widget que se puede integrar en el escritorio
  */
 public class StationWidget extends AppWidgetProvider {
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-                         int[] appWidgetIds) {
-        //Log.d("OTempo", "WIDGET ON UPDATE");
-        final int N = appWidgetIds.length;
-
+    public void onUpdate(
+            Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // Si no conseguimos elegir primera estación, entonces ponemos el mensaje de "cargando..."
         if (!firstStation(context)) {
             // Actualizamos todos los widgets (va a ser uno solo) con el layout por defecto
             for (int appWidgetId : appWidgetIds) {
-                RemoteViews rviews = new RemoteViews(context.getPackageName(), R.layout.widget_layout_err);
+                RemoteViews rviews =
+                        new RemoteViews(context.getPackageName(), R.layout.widget_layout_err);
                 rviews.setTextViewText(R.id.widgetError, context.getString(R.string.loading_data));
 
                 Intent otempoIntent = new Intent(context, StationActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, otempoIntent, 0);
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(context, 0, otempoIntent, 0);
                 rviews.setOnClickPendingIntent(R.id.widgetRoot, pendingIntent);
                 appWidgetManager.updateAppWidget(appWidgetId, rviews);
             }
@@ -72,11 +72,15 @@ public class StationWidget extends AppWidgetProvider {
      */
     private static boolean firstStation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        String defaultStationPreference = prefs.getString(Preferences.PREF_DEFAULT_STATION, Preferences.DEFAULT_DEFAULT_STATION);
-        int defaultStationFixed = Integer.valueOf(prefs.getString(Preferences.PREF_DEFAULT_STATION_FIXED, "1"));
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        String defaultStationPreference = prefs.getString(
+                Preferences.PREF_DEFAULT_STATION, Preferences.DEFAULT_DEFAULT_STATION);
+        int defaultStationFixed =
+                Integer.valueOf(prefs.getString(Preferences.PREF_DEFAULT_STATION_FIXED, "1"));
+        LocationManager locationManager =
+                (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         assert locationManager != null;
-        StationManager tmpStationManager = new StationManager(locationManager, defaultStationPreference, defaultStationFixed);
+        StationManager tmpStationManager =
+                new StationManager(locationManager, defaultStationPreference, defaultStationFixed);
         Station station = tmpStationManager.getStation();
         if (station == null) {
             station = Station.getKnownStations().get(0);
@@ -94,8 +98,8 @@ public class StationWidget extends AppWidgetProvider {
      * @return Devuelve la predicción para hoy de las que contiene una estación
      */
     private static StationPrediction getTodayPrediction(Station station) {
-        for (StationPrediction prediction: station.getPredictions()) {
-        	final Calendar predictionDate = prediction.getDate();
+        for (StationPrediction prediction : station.getPredictions()) {
+            final Calendar predictionDate = prediction.getDate();
             if (predictionDate != null && DateUtils.isFromToday(predictionDate)) {
                 return prediction;
             }
@@ -105,6 +109,7 @@ public class StationWidget extends AppWidgetProvider {
 
     /**
      * Actualiza el widget con la información de la estación proporcionada
+     *
      * @param context Contexto válido de Android
      * @param station La estación que deseamos mostrar en el widget
      */
@@ -119,22 +124,33 @@ public class StationWidget extends AppWidgetProvider {
         }
         StationPrediction prediction = getTodayPrediction(station);
         if (prediction == null) {
-            rviews = new RemoteViews(context.getPackageName(),  R.layout.widget_layout_err);
+            rviews = new RemoteViews(context.getPackageName(), R.layout.widget_layout_err);
             rviews.setTextViewText(R.id.widgetError, context.getString(R.string.internet_error));
         } else {
             if (prediction instanceof StationShortTermPrediction) {
-                StationShortTermPrediction pred = (StationShortTermPrediction)prediction;
-                rviews = new RemoteViews(context.getPackageName(),  R.layout.widget_layout);
-                rviews.setTextViewText(R.id.widgetTemps, pred.getMinTemp()+" - "+pred.getMaxTemp() + " C");
-                rviews.setImageViewResource(R.id.widgetMorning, ResourceUtils.getResource(pred.getSkyStateMorning(), true));
-                rviews.setImageViewResource(R.id.widgetAfternoon, ResourceUtils.getResource(pred.getSkyStateAfternoon(), true));
-                rviews.setImageViewResource(R.id.widgetEvening, ResourceUtils.getResource(pred.getSkyStateNight(), false));
+                StationShortTermPrediction pred = (StationShortTermPrediction) prediction;
+                rviews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+                rviews.setTextViewText(
+                        R.id.widgetTemps,
+                        pred.getMinTemp() + " - " + pred.getMaxTemp() + " C");
+                rviews.setImageViewResource(
+                        R.id.widgetMorning,
+                        ResourceUtils.getResource(pred.getSkyStateMorning(), true));
+                rviews.setImageViewResource(
+                        R.id.widgetAfternoon,
+                        ResourceUtils.getResource(pred.getSkyStateAfternoon(), true));
+                rviews.setImageViewResource(
+                        R.id.widgetEvening,
+                        ResourceUtils.getResource(pred.getSkyStateNight(), false));
             } else if (prediction instanceof StationMediumTermPrediction) {
-                StationMediumTermPrediction predMed = (StationMediumTermPrediction)prediction;
-                rviews = new RemoteViews(context.getPackageName(),  R.layout.widget_layout_medium_term);
-                rviews.setImageViewResource(R.id.widgetMorning, ResourceUtils.getResource(predMed.getSkyState(), true));
+                StationMediumTermPrediction predMed = (StationMediumTermPrediction) prediction;
+                rviews = new RemoteViews(
+                        context.getPackageName(), R.layout.widget_layout_medium_term);
+                rviews.setImageViewResource(
+                        R.id.widgetMorning,
+                        ResourceUtils.getResource(predMed.getSkyState(), true));
             } else {
-                rviews = new RemoteViews(context.getPackageName(),  R.layout.widget_layout_err);
+                rviews = new RemoteViews(context.getPackageName(), R.layout.widget_layout_err);
                 rviews.setTextViewText(R.id.widgetError, context.getString(R.string.internal_error));
             }
         }
@@ -145,7 +161,8 @@ public class StationWidget extends AppWidgetProvider {
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
 
         Intent otempoIntent = new Intent(context, StationActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, otempoIntent, 0);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, 0, otempoIntent, 0);
         rviews.setOnClickPendingIntent(R.id.widgetRoot, pendingIntent);
         manager.updateAppWidget(widget, rviews);
     }
